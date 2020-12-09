3eae985e5f93fe3e3ae6cf8244a74870778e0ca7
Gaurav <aadeezcity@gmail.com>
2020-12-09 21:45:17 +0530

    when dino and obsticle will come near to each other till they collapse then we will show game over msg

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ document.onkeydown = function(e){
     }
 }
 
+setInterval(() => {
+    dino = document.querySelector(".dino");
+    gameOver = document.querySelector(".gameOver");
+    obstacle = document.querySelector(".obstacle");
+
+    Dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+    Dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+    
+    Ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left')); //distance from lest
+    Oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top')); //distance from top
+
+    offSetX = Math.abs(Dx - Ox); //203
+    offSetY = Math.abs(Dy - Oy);
+
+   
+
+    if(offSetX <  93 && offSetY < 52){
+        gameOver.style.visibility = "visible";
+        obstacle.classList.remove('obstacleAni');
+    }
+
+    
+    
+
+}, 100 );
+
+//37 = left 
+//39 =right
