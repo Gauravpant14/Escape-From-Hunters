a604e0b9b50363c648722d200b5b4866451f4cb7
Gaurav <aadeezcity@gmail.com>
2020-12-10 14:05:33 +0530

    Now we can move dino forward and backward & score is also visible but need to fix one issue with score board

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+let score = 0;
+
+
 document.onkeydown = function(e){
-    console.log("hello",e.keyCode);
-  
+    
     if(e.keyCode == 38){
      dino = document.querySelector(".dino");
         dino.classList.add('animateDino');
         setTimeout(() => {
             dino.classList.remove('animateDino');
         }, 700);
+    }
+
+    //move dino ahead
+    if(e.keyCode == 39){
+        dino = document.querySelector(".dino");
+        dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+
+    //move dino backward
+    if(e.keyCode == 37){
+        dino = document.querySelector(".dino");
+        dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = dinoX - 112 + "px"; 
+
     }
 }
 
@@ -24,17 +41,23 @@ setInterval(() => {
     offSetX = Math.abs(Dx - Ox); //203
     offSetY = Math.abs(Dy - Oy);
 
-   
-
     if(offSetX <  93 && offSetY < 52){
         gameOver.style.visibility = "visible";
         obstacle.classList.remove('obstacleAni');
+    } 
+
+    else {
+        score+=1 ;
+        updateScore(score);
     }
 
-    
-    
-
 }, 100 );
+
+function updateScore(score) {
+   
+    scoreCon.innerHTML = `your score is :${score}`;
+    
+}
 
 //37 = left 
 //39 =right
