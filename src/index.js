788e958a8418a20201110f9338b888888a766521
Gaurav <aadeezcity@gmail.com>
2020-12-15 14:17:47 +0530

    score board isse resolved, Music also added, need more improvements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 let score = 0;
+let cross = true;
+
+audio = new Audio('../audio/music.mp3');
+soundGover = new Audio('../audio/gameover.mp3');
+
+setTimeout(() => {
+    audio.play();
+}, 1000);
 
 
 document.onkeydown = function(e){
@@ -32,23 +40,33 @@ setInterval(() => {
     gameOver = document.querySelector(".gameOver");
     obstacle = document.querySelector(".obstacle");
 
-    Dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    Dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+    Dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left')); //dino distance from left
+    Dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top')); //dino distance from top
     
-    Ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left')); //distance from lest
-    Oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top')); //distance from top
+    Ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left')); //obsticle distance from lest
+    Oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top')); //obstacle distance from top
 
-    offSetX = Math.abs(Dx - Ox); //203
+    offSetX = Math.abs(Dx - Ox); 
     offSetY = Math.abs(Dy - Oy);
 
     if(offSetX <  93 && offSetY < 52){
-        gameOver.style.visibility = "visible";
+        gameOver.innerHTML = "Game Over";
         obstacle.classList.remove('obstacleAni');
+        soundGover.play();
+        setTimeout(() => {
+            soundGover.pause();
+            audio.pause();
+        }, 1000);
+        
     } 
 
-    else {
+    else if(offSetX < 145 && cross) {
         score+=1 ;
         updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
     }
 
 }, 100 );
@@ -58,6 +76,8 @@ function updateScore(score) {
     scoreCon.innerHTML = `your score is :${score}`;
     
 }
+
+
 
 //37 = left 
 //39 =right
